d1efc8357ef893a0c8ebd90568579d8129934923
ETBlue <etblue.tw@gmail.com>
2013-10-19 22:39:52 +0800

    update jade template

--- http_servlet_handler.rb
+++ http_servlet_handler.rb
@@ -1,24 +1,37 @@
-class JadeHandler
-  def initialize(app)
-    @app = app
-  end
- 
-  def call(env)
+require 'open3'
+
+class JadeHandler 
+  def initialize(app)  
+    @app = app  
+  end  
+
+  def call(env) 
     if env["PATH_INFO"] =~ /\/$/
       env["PATH_INFO"] += "index.jade"
     end
- 
+
     if env["PATH_INFO"] =~ /\.jade$/
-     path =  env["PATH_INFO"][1..-1]
- 
-      body = %x{jade --path . < #{path} }
- 
+      path =  env["PATH_INFO"][1..-1]
+      base = File.basename(path)
+      dir = File.dirname(path) + "/src"
+      body = nil
+
+      Dir.chdir(dir) do
+        body = Open3.popen3('node c:/users/etblue/appdata/roaming/npm/node_modules/jade/bin/jade --path .') do |stdin, stdout, stderr|
+          template = open(base,'r'){|f| f.read}
+
+          stdin.write template
+          stdin.close
+
+          stdout.read + stderr.read.gsub(/\n/, '<br>')
+        end
+      end
       [200, {"Content-Type" => "text/html"}, [body]]
     else
       status, headers, body = @app.call(env)
       [status, headers, body]
-    end
-  end
+    end 
+  end  
 end
- 
+
 use JadeHandler
