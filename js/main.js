958a821e86e07be187865c62947df48dfdcb0213
ETBlue <etblue.tw@gmail.com>
2013-10-22 22:02:34 +0800

    remove author & update idea page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 $('.ui.dropdown').dropdown({'on':'hover'});
+
+$('.ui.shape')
+  .shape()
+;
+
+/*
+$('.ui.shape')
+  .shape('set next side','.draft')
+  .shape('flip left')
+;
+
+$('.ui.shape')
+  .shape('set next side','.final')
+  .shape('flip right')
+;
+*/
